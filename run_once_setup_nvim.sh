8fe2d1192213d2972ad6bf1eaaa7da343dd36c77
Gustavo R. <gu.does.dev@gmail.com>
2025-06-13 09:32:20 -0300

    changing kickstart.nvim to kickstart-modular.nvim

--- run_once_setup_nvim.sh
+++ run_once_setup_nvim.sh
@@ -6,5 +6,5 @@ if [ -d "$NVIM_CONFIG_DIR" ]; then
   echo "nvim configuration directory already exists at $NVIM_CONFIG_DIR. Skipping clone."
 else
   echo "Trying to clone kickstart.nvim"
-  git clone https://github.com/nvim-lua/kickstart.nvim.git "$NVIM_CONFIG_DIR"
+  git clone https://github.com/dam9000/kickstart-modular.nvim.git "$NVIM_CONFIG_DIR"
 fi
